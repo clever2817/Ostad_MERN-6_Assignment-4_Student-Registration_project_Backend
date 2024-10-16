cb79c9358c743f548c1d7a4f5bbe7fb957b7bd39
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-10-16 19:44:59 +0600

    Student Schema
    Student Schema

---
+++ student-registration/models/Student.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePic: { type: String }, // For file uploads
+});
+
+const Student = mongoose.model("Student", studentSchema);
+module.exports = Student;
